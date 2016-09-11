ecaf586b5b33b7d07b7eb358e25dba91bd54f417
Jelmer Schreuder <j.schreuder@mijnpraktijk.com>
2016-09-11 17:20:42 +0200

    Added first spec for ControllerRunner

---
+++ spec/ControllerRunnerSpec.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace spec\jschreuder\Middle;
+
+use jschreuder\Middle\ControllerRunner;
+use jschreuder\Middle\View\RendererInterface;
+use jschreuder\Middle\View\ViewInterface;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+/** @mixin  ControllerRunner */
+class ControllerRunnerSpec extends ObjectBehavior
+{
+    /** @var  RendererInterface */
+    private $renderer;
+
+    public function let(RendererInterface $renderer)
+    {
+        $this->renderer = $renderer;
+        $this->beConstructedWith($renderer);
+    }
+
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType(ControllerRunner::class);
+    }
+
+    public function it_can_execute_a_controller(
+        ServerRequestInterface $request,
+        ViewInterface $view,
+        ResponseInterface $response
+    )
+    {
+        $controller = function () use ($view) {
+            return $view->getWrappedObject();
+        };
+        $request->getAttribute('controller')->willReturn($controller);
+        $this->renderer->render($request, $view)->willReturn($response);
+        $this->execute($request)->shouldReturn($response);
+    }
+
+    public function it_can_execute_a_controller_returning_a_psr_response(
+        ServerRequestInterface $request,
+        ResponseInterface $response
+    )
+    {
+        $controller = function () use ($response) {
+            return $response->getWrappedObject();
+        };
+        $request->getAttribute('controller')->willReturn($controller);
+        $this->execute($request)->shouldReturn($response);
+    }
+
+    public function it_will_error_on_invalid_controller(ServerRequestInterface $request)
+    {
+        $request->getAttribute('controller')->willReturn('trim');
+        $this->shouldThrow(\RuntimeException::class)->duringExecute($request);
+    }
+
+    public function it_will_error_on_invalid_response(ServerRequestInterface $request)
+    {
+        $controller = function () {
+            return 'an invalid response';
+        };
+        $request->getAttribute('controller')->willReturn($controller);
+        $this->shouldThrow(\TypeError::class)->duringExecute($request);
+    }
+}
