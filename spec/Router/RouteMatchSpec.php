dd29de36c19e189806519a11e64590a0cd20f472
Jelmer Schreuder <j.schreuder@mijnpraktijk.com>
2016-09-11 18:18:52 +0200

    Spec'ed RouteMatch

---
+++ spec/Router/RouteMatchSpec.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace spec\jschreuder\Middle\Router;
+
+use jschreuder\Middle\Router\RouteMatch;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+/** @mixin  RouteMatch */
+class RouteMatchSpec extends ObjectBehavior
+{
+    public function it_is_initializable_as_no_match()
+    {
+        $this->beConstructedWith(false);
+        $this->shouldHaveType(RouteMatch::class);
+        $this->isMatch()->shouldReturn(false);
+    }
+
+    public function it_is_initializable_as_match()
+    {
+        $controller = function () {};
+        $attributes = ['answer' => 42];
+        $this->beConstructedWith(true, $controller, $attributes);
+        $this->shouldHaveType(RouteMatch::class);
+        $this->isMatch()->shouldReturn(true);
+        $this->getController()->shouldReturn($controller);
+        $this->getAttributes()->shouldReturn($attributes);
+    }
+
+    public function it_cannot_be_a_match_without_controller()
+    {
+        $this->beConstructedWith(true);
+        $this->shouldThrow(\RuntimeException::class)->duringInstantiation();
+    }
+}
