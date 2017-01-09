616bb541b676eb3407e8b0e0f646fd2fb1c01e28
Jelmer Schreuder <j.schreuder@mijnpraktijk.com>
2017-01-09 17:12:24 +0100

    Added JSON body parser.

---
+++ spec/ServerMiddleware/JsonRequestParserMiddlewareSpec.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace spec\jschreuder\Middle\ServerMiddleware;
+
+use Interop\Http\ServerMiddleware\DelegateInterface;
+use jschreuder\Middle\ServerMiddleware\JsonRequestParserMiddleware;
+use PhpSpec\ObjectBehavior;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\StreamInterface;
+
+/** @mixin  JsonRequestParserMiddleware */
+class JsonRequestParserMiddlewareSpec extends ObjectBehavior
+{
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType(JsonRequestParserMiddleware::class);
+    }
+
+    public function it_can_parse_json(
+        ServerRequestInterface $request1,
+        ServerRequestInterface $request2,
+        StreamInterface $body,
+        DelegateInterface $delegate,
+        ResponseInterface $response
+    )
+    {
+        $array = ['test' => 42, 'next' => 'best'];
+        $body->getContents()->willReturn(json_encode($array));
+        $request1->getBody()->willReturn($body);
+        $request1->getHeaderLine('Content-Type')->willReturn('application/json');
+        $request1->withParsedBody($array)->willReturn($request2);
+
+        $delegate->process($request2)->willReturn($response);
+
+        $this->process($request1, $delegate)->shouldReturn($response);
+    }
+
+    public function it_can_parse_non_default_json_content_types(
+        ServerRequestInterface $request1,
+        ServerRequestInterface $request2,
+        StreamInterface $body,
+        DelegateInterface $delegate,
+        ResponseInterface $response
+    )
+    {
+        $this->beConstructedWith(['application/vnd.api+json', 'application/json']);
+
+        $array = ['test' => 42, 'next' => 'best'];
+        $body->getContents()->willReturn(json_encode($array));
+        $request1->getBody()->willReturn($body);
+        $request1->getHeaderLine('Content-Type')->willReturn('application/vnd.api+json');
+        $request1->withParsedBody($array)->willReturn($request2);
+
+        $delegate->process($request2)->willReturn($response);
+
+        $this->process($request1, $delegate)->shouldReturn($response);
+    }
+
+    public function it_can_do_nothing(
+        ServerRequestInterface $request,
+        DelegateInterface $delegate,
+        ResponseInterface $response
+    )
+    {
+        $request->getHeaderLine('Content-Type')->willReturn('text/html');
+        $delegate->process($request)->willReturn($response);
+        $this->process($request, $delegate)->shouldReturn($response);
+    }
+}
---
+++ src/ServerMiddleware/JsonRequestParserMiddleware.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types = 1);
+
+namespace jschreuder\Middle\ServerMiddleware;
+
+
+use Interop\Http\ServerMiddleware\DelegateInterface;
+use Interop\Http\ServerMiddleware\MiddlewareInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class JsonRequestParserMiddleware implements MiddlewareInterface
+{
+    /** @var  string[] */
+    private $jsonContentTypes;
+
+    public function __construct(array $jsonContentTypes = null)
+    {
+        if (is_null($jsonContentTypes)) {
+            $jsonContentTypes = ['application/json'];
+        }
+        $this->jsonContentTypes = $jsonContentTypes;
+    }
+
+    public function process(ServerRequestInterface $request, DelegateInterface $delegate): ResponseInterface
+    {
+        if (in_array($request->getHeaderLine('Content-Type'), $this->jsonContentTypes, true)) {
+            $request = $request->withParsedBody($this->parseBody($request));
+        }
+        return $delegate->process($request);
+    }
+
+    private function parseBody(ServerRequestInterface $request) : array
+    {
+        $parsedBody = json_decode($request->getBody()->getContents(), true);
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new \InvalidArgumentException('Could not decode JSON body: ' . json_last_error_msg());
+        }
+
+        return $parsedBody;
+    }
+}
