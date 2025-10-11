5a0d5020a5d20e1b597361f16420ac6f4e98f868
Jelmer Schreuder <github@jelmerschreuder.nl>
2025-10-11 11:10:10 +0200

    Made Viewrenderer integration test more of an integration test

--- tests/Integration/ViewRenderingTest.php
+++ tests/Integration/ViewRenderingTest.php
@@ -4,6 +4,9 @@ use jschreuder\Middle\View\View;
 use jschreuder\Middle\View\RedirectView;
 use jschreuder\Middle\View\TwigRenderer;
 use jschreuder\Middle\View\RedirectRendererMiddleware;
+use Laminas\Diactoros\ResponseFactory;
+use Laminas\Diactoros\ServerRequest;
+use Laminas\Diactoros\Uri;
 use Twig\Environment;
 use Twig\Loader\ArrayLoader;
 
@@ -13,41 +16,31 @@ test('it can render a view through the full rendering pipeline', function () {
     ]);
     $twig = new Environment($loader);
     
-    $responseFactory = Mockery::mock(\Psr\Http\Message\ResponseFactoryInterface::class);
-    $response = Mockery::mock(\Psr\Http\Message\ResponseInterface::class);
-    $stream = Mockery::mock(\Psr\Http\Message\StreamInterface::class);
-    
-    $responseFactory->shouldReceive('createResponse')->andReturn($response);
-    $response->shouldReceive('withHeader')->andReturnSelf();
-    $response->shouldReceive('getBody')->andReturn($stream);
-    $stream->shouldReceive('rewind')->twice();
-    $stream->shouldReceive('write')->with('Hello World!')->once();
+    $responseFactory = new ResponseFactory();
     
     $renderer = new TwigRenderer($twig, $responseFactory);
     $redirectRenderer = new RedirectRendererMiddleware($renderer, $responseFactory);
     
     $view = new View('test.twig', ['name' => 'World']);
-    $request = Mockery::mock(\Psr\Http\Message\ServerRequestInterface::class);
+    $request = new ServerRequest([], [], new Uri('http://localhost/twig'), 'GET');
     
     $result = $redirectRenderer->render($request, $view);
     expect($result)->toBeInstanceOf(\Psr\Http\Message\ResponseInterface::class);
+    expect($result->getBody()->getContents())->toBe('Hello World!');
 });
 
 test('it can handle redirects through the rendering pipeline', function () {
     $innerRenderer = Mockery::mock(\jschreuder\Middle\View\RendererInterface::class);
-    $responseFactory = Mockery::mock(\Psr\Http\Message\ResponseFactoryInterface::class);
-    $response = Mockery::mock(\Psr\Http\Message\ResponseInterface::class);
-    
-    $responseFactory->shouldReceive('createResponse')->with(302)->andReturn($response);
-    $response->shouldReceive('withHeader')->with('Location', '/dashboard')->andReturnSelf();
-    
     $innerRenderer->shouldNotReceive('render');
+
+    $responseFactory = new ResponseFactory();
     
     $renderer = new RedirectRendererMiddleware($innerRenderer, $responseFactory);
     
     $view = new RedirectView('/dashboard', 302);
-    $request = Mockery::mock(\Psr\Http\Message\ServerRequestInterface::class);
+    $request = new ServerRequest([], [], new Uri('http://localhost/redirect'), 'GET');
     
     $result = $renderer->render($request, $view);
     expect($result)->toBeInstanceOf(\Psr\Http\Message\ResponseInterface::class);
+    expect($result->getHeaderLine('Location'))->toBe('/dashboard');
 });
